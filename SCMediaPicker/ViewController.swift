ea7d51a391846eb7dccf74d65b94738399d4897a
Glenn Posadas <hello@glennvon.com>
2024-10-01 04:41:18 +0800

    add base proj

---
+++ SCMediaPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCMediaPicker
+//
+//  Created by Glenn Posadas on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SCMediaPickerUITests/SCMediaPickerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SCMediaPickerUITestsLaunchTests.swift
+//  SCMediaPickerUITests
+//
+//  Created by Glenn Posadas on 10/1/24.
+//
+
+import XCTest
+
+final class SCMediaPickerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
