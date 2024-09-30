1f6b449185a4ca5689ad0e94bdf91cf2c4984951
Glenn Posadas <hello@glennvon.com>
2024-10-01 06:05:48 +0800

    demo stuff

--- SCMediaPicker-Demo/ViewController.swift
+++ SCMediaPicker-Demo/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startPicker(_ sender: Any) {
+        
+    }
+    
 }
 
